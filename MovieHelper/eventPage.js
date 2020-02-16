deca34c7239e03cdbb7ff6b0bbcdb58fe691c1c2
Lukasz-Schulz <lukasz.h.schulz@gmail.com>
2020-02-16 21:19:46 +0100

    dynamically added pages

--- MovieHelper/eventPage.js
+++ MovieHelper/eventPage.js
@@ -4,22 +4,16 @@ var contextMenuItem = {
     "contexts": ["selection"]
 };
 
-let pages = ["rotten tomatoes", "filmweb"];
-
 
 chrome.contextMenus.create(contextMenuItem);
 
 chrome.contextMenus.onClicked.addListener(function(selected){
-    alert(3);
     let searchTerm;
     if (selected.menuItemId == "goToReviews" && selected.selectionText){
         searchTerm = selected.selectionText;
         let pages = [];
-        alert(4);
         pages = JSON.parse(localStorage.getItem('pages'));
-        alert(pages);
         for(page of pages) {
-            alert(page);
             let url =  `http://www.google.com/search?q=${page} ${searchTerm}&btnI`;
             openInNewTab(url);
         }
@@ -28,5 +22,5 @@ chrome.contextMenus.onClicked.addListener(function(selected){
 
 function openInNewTab(url) {
     var win = window.open(url);
-    win.focus();
+    // win.focus();
   }
--- MovieHelper/popup.js
+++ MovieHelper/popup.js
@@ -1,17 +1,27 @@
 $(function(){
+    let storage = localStorage.getItem('pages');
+    if(storage) {
+        for(page of JSON.parse(storage)){
+            let node = document.createElement("LI");
+            let textnode = document.createTextNode(page);
+            node.append(textnode);
+            $('#pages').append(node);
+        
+        }
+    }
+
     $("#add").click(function(){
-            let list = localStorage.getItem('pages');
-            alert(list);
+            let list = JSON.parse(localStorage.getItem('pages'));
             let newList = [];
             if(list) {
-                newList.push(list);
+                newList = list;
             }
 
             let newPage = $("#name").val();
-            alert(newPage);
             if(newPage){                
-                newList.push(newPage);
+                newList[newList.length] = newPage;
             }
+            localStorage.clear();
             localStorage.setItem('pages', JSON.stringify(newList));
         })
     });
